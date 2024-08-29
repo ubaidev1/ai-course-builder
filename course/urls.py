7aaed76657cf68cc39c5abc2d18fcb1abe556b32
ubaidev1 <ubaidev01@gmail.com>
2024-08-29 19:14:59 +0500

    rewrite changes

--- course/urls.py
+++ course/urls.py
@@ -5,8 +5,7 @@ from .views import *
 urlpatterns = [
     path('home/', home_view, name='home'),
     path('course/<uuid:course_id>/', course_detail_view, name='course_detail'),
-    path('course/<uuid:course_id>/module/<uuid:module_id>/', module_detail_view, name='module_detail'),
-    path('course/<uuid:course_id>/lesson/<uuid:lesson_id>/', lesson_detail_view,
+    path('course/<uuid:course_id>/lesson/<uuid:lesson_id>/', course_detail_view,
          name='lesson_detail'),
     path('quiz/<uuid:lesson_id>/', quiz_view, name='quiz_view'),
     path('submit_quiz_score/<uuid:lesson_id>/', submit_quiz_score, name='submit_quiz_score'),
