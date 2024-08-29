6f5e289723a755ee6fabe09f24e056a99a4c523f
ubaidev1 <ubaidev01@gmail.com>
2024-08-29 19:33:43 +0500

    latest code

--- antropic_api/anthropic_response.py
+++ antropic_api/anthropic_response.py
@@ -30,7 +30,14 @@ def extract_text_from_pdf(pdf_path):
 
 def create_prompt_from_template(pdf_text, prompt_template):
     """Combine the prompt template with the PDF content."""
-    return f"{prompt_template}\n\nPDF Content:\n{pdf_text}"
+    return f"""
+                   Here is the content of the PDF:
+                   <pdf_content>
+                   {pdf_text}
+                   </pdf_content>
+                   {prompt_template}
+               """
+
 
 
 def send_prompt_to_claude(client, prompt, model, max_tokens, temperature, system):
@@ -93,7 +100,6 @@ def get_ai_course_details(config_file, pdf_file_path):
 
     # Extract and parse the JSON from Claude's response
     return extract_json_from_message(message)
-
 
 # Run the main function
 # if __name__ == "__main__":
