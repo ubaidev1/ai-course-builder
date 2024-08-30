c56432514f2033808a713b852f29833683f53662
ubaidev1 <ubaidev01@gmail.com>
2024-08-30 17:28:42 +0500

    latest code

--- course/urls.py
+++ course/urls.py
@@ -4,8 +4,9 @@ from .views import *
 
 urlpatterns = [
     path('home/', home_view, name='home'),
-    path('course/<uuid:course_id>/', course_detail_view, name='course_detail'),
-    path('course/<uuid:course_id>/lesson/<uuid:lesson_id>/', course_detail_view,
-         name='lesson_detail'),
-    path('submit_quiz_score/<uuid:lesson_id>/', submit_quiz_score, name='submit_quiz_score'),
+    path('next_lesson/<uuid:course_id>/', next_lesson_view, name='next_lesson'),
+
+    path('course/<uuid:course_id>/lesson/<uuid:lesson_id>/', course_detail_view, name='course_detail'),
+
+    path('submit_quiz_score/<uuid:course_id>/<uuid:lesson_id>/', submit_quiz_score, name='submit_quiz_score'),
 ]
