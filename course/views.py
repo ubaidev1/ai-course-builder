60e33dfd0b143b4d55b7be7c31cfbfb8b774103b
ubaidev1 <ubaidev01@gmail.com>
2024-09-10 15:08:40 +0500

    show only course for which get invitation

--- course/views.py
+++ course/views.py
@@ -115,7 +115,7 @@ def get_next_lesson(course, user):
 
 
 def get_course_progress(user):
-    courses = Course.objects.filter(is_published=True)
+    courses = Course.objects.filter(is_published=True, enrollment__user=user)
     course_progress = []
     for course in courses:
         total_quizzes = Quiz.objects.filter(lesson__module__course=course).count()
