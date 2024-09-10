b4b3ea66510dd6e11c660912c57027e3210de844
ubaidev1 <ubaidev01@gmail.com>
2024-09-10 18:55:25 +0500

    add two urls edit course and update course

--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-
 from .views import *
 
 urlpatterns = [
     path('dashboard/', dashboard, name='dashboard'),
+    path('edit_courses/', edit_courses, name='edit_courses'),
     path('course_actions/', course_actions, name="course_actions"),
     path('toggle-course-publish/<uuid:course_id>/', toggle_course_publish, name='toggle_course_publish'),
+    path('update-course/<uuid:course_id>/', update_course, name='update_course'),  # Added this line
     path('result/', result_view, name='result'),
     path('extend_course/', extend_course, name='extend_course'),
     path('', signup, name='signup'),
