b1861ea0651b7ce8019d67b3390000515479d93f
ubaidev1 <ubaidev01@gmail.com>
2024-09-25 13:49:59 +0500

    include middle ware in settings

--- coursebuilder/settings.py
+++ coursebuilder/settings.py
@@ -35,6 +35,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'users.middleware.CustomizationMiddleware',
+
 ]
 
 ROOT_URLCONF = 'coursebuilder.urls'
